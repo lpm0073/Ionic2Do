680f3df6c0e1757011d06ebd7d3f09ca778d19e4
Lawrence McDaniel <lpm0073@gmail.com>
2017-05-03 09:11:02 -0500

    first commit

---
+++ src/pages/tasklist/tasklist.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-tasklist',
+  templateUrl: 'tasklist.html'
+})
+export class TaskListPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
